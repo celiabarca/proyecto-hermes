cea1088bcc3ed6a4baca95b6bf011748edd8f0b2
DanielDAW2 <rt.daniel.1994@gmail.com>
2018-05-10 18:41:37 +0200

    MergeConflict

--- src/Controller/ProyectController.php
+++ src/Controller/ProyectController.php
@@ -29,16 +29,33 @@ use App\Form\ProjectType;
             $entityManager->persist($projecto);
             $entityManager->flush();
             
+            return $this->redirectToRoute("index");
         }
+        return $this->render('project/nuevo.html.twig', [
+            'FormularioProyecto' => $formularioProyecto->createView()
+        ]);
     }
     
 <<<<<<< HEAD
     static public function dameProyectos($total)
 =======
+    public function dameProyectos($total = null)
 >>>>>>> bf9316992ad9f7ef0c555e7a6c73d78cbf3076db
     {
+        if (isset($total) && $total > 0) {
+            $proyectos = $this->getDoctrine()
+                            ->getRepository(Project::class)
+                            ->findBy([], null, $total);
+        } else {
+            $proyectos = $this->getDoctrine()->getRepository(Project::class)->findAll();
         }
         
         return $proyectos;
+    }
+
+    public function proyecto(Project $proyecto) {
+        return $this->render('project/index.html.twig', [
+            'proyecto' => $proyecto
+        ]);
     }
 }
