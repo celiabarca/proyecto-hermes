0b82738de930d9da69f316185f227be6756f9855
MagonxESP <janma.360@gmail.com>
2018-05-14 20:18:14 +0200

    Acciones de seguimiento y formulario para añadir un seguimiento

--- src/Controller/SeguimientosController.php
+++ src/Controller/SeguimientosController.php
@@ -4,25 +4,55 @@ namespace App\Controller;
 
 use App\Entity\Project;
 use App\Entity\Seguimiento;
+use App\Form\SeguimientoType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class SeguimientosController extends Controller
 {
     /**
-     * Añade una fase del seguimiento del proyecto
+     * Renderiza la plantilla que muestra los seguimientos
      * @param Project $proyecto
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function anyadirSeguimiento(Project $proyecto) {
+    public function seguimientos(Project $proyecto) {
+        return $this->render('seguimientos/index.html.twig', [
+            'proyecto' => $proyecto,
+        ]);
+    }
+
+    /**
+     * Añade una fase del seguimiento del proyecto
+     * @param Request $request
+     * @param Project $proyecto
+     * @return mixed
+     */
+    public function anyadirSeguimiento(Request $request, Project $proyecto) {
         $seguimiento = new Seguimiento();
 
-        // TODO implementar en formulario
-        $seguimiento->setProyecto($proyecto);
-        $seguimiento->setUsuario($this->getUser());
-        $seguimiento->setFecha(new \DateTime());
-        $manager = $this->getDoctrine()->getManager();
-        $manager->persist($seguimiento);
-        $manager->flush();
+        $form = $this->createForm(SeguimientoType::class, $seguimiento, [
+            'action' => '/proyecto/'.$proyecto->getId().'/seguimiento/nuevo'
+        ]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $seguimiento->setProyecto($proyecto);
+            $seguimiento->setUsuario($this->getUser());
+            $seguimiento->setFecha(new \DateTime());
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($seguimiento);
+            $manager->flush();
+
+            return $this->redirectToRoute('Seguimientos', [
+                'id' => $proyecto->getId()
+            ]);
+        }
+
+        return $this->render('seguimientos/seguimiento-form.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
@@ -35,6 +65,12 @@ class SeguimientosController extends Controller
     public function eliminarSeguimiento(Project $project, Seguimiento $seguimiento) {
         try {
             $eliminado = false;
+
+            $usuario = $this->getUser();
+
+            if($project->getAutor() !== $usuario) {
+                throw new \Exception('Este proyecto no es tuyo!');
+            }
 
             if($project->getSeguimientos()->contains($seguimiento)) {
                 $manager = $this->getDoctrine()->getManager();
@@ -51,5 +87,21 @@ class SeguimientosController extends Controller
                 'error' => $e->getMessage()
             ], 405);
         }
+    }
+
+    // TODO acabar de repantear lo de actualizar el seguimiento
+    public function actualizarSeguimiento(Request $request, Seguimiento $seguimiento) {
+        $form = $this->createForm(SeguimientoType::class, $seguimiento);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($seguimiento);
+            $manager->flush();
+        }
+
+        return $this->render('seguimiento-form.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/SeguimientoType.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: magonxesp
+ * Date: 14/05/18
+ * Time: 17:57
+ */
+
+namespace App\Form;
+
+
+use App\Entity\Seguimiento;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeguimientoType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('situacion', TextType::class, [
+                'label' => 'Situacion del proyecto'
+            ])
+            ->add('Enviar', SubmitType::class, [
+                'label' => 'Enviar nueva situacion'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            'data_class' => Seguimiento::class
+        ]);
+    }
+
+}
