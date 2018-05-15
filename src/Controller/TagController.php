59ae0c5a20b96d5c32f8d0c1933c2f03f05f5ae9
MagonxESP <janma.360@gmail.com>
2018-05-15 19:46:00 +0200

    query para ver tags por nombre parecido

---
+++ src/Controller/TagController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: magonxesp
+ * Date: 15/05/18
+ * Time: 19:04
+ */
+
+namespace App\Controller;
+
+use App\Repository\TagRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class TagController extends Controller {
+
+    private $tagrepository;
+
+    public function __construct(TagRepository $repository) {
+        $this->tagrepository = $repository;
+    }
+
+    /**
+     * Preparado para AJAX
+     * Devuelve todos los tags con un nombre parecido
+     * @param string $nombre
+     * @return JsonResponse
+     */
+    public function getTags(string $nombre) {
+        $tags = $this->tagrepository->findByLikeName($nombre);
+        $tagnames = [];
+
+        foreach($tags as $tag) {
+            $tagnames[] = $tag->getNombre();
+        }
+
+        return new JsonResponse([
+            'tags' => $tagnames
+        ]);
+    }
+
+}
