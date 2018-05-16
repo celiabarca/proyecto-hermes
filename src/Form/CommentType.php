900f96c46dce34e14495b66aa16c521e2a1ae4a0
barcacelia <barca.celia23@gmail.com>
2018-05-16 16:42:55 +0200

    empezr loging

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -9,13 +9,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use \Symfony\Component\Form\Extension\Core\Type\TextareaType;
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('contenido', TextType::class)
+            ->add('contenido',TextareaType::class)
                 ->add('Enviar',SubmitType::class)
         ;
     }
