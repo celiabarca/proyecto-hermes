46160249fb66c1bceff09a5d9f45b7535c473485
draya <draya@daniel-fd>
2018-05-29 16:58:34 +0200

    Limit showing proyects on the homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller {
      * @return mixed
      */
     public function index() {
-        $proyectos = $this->pc->dameProyectos();
+        $proyectos = $this->pc->dameProyectos(2);
      
         $form = $this->createForm(RegisterType::class);
         $user = $this->userRepository->findAll();
