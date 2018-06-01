83df97717d828a53ebf9d1456a825c214d9d6b74
barcacelia <barca.celia23@gmail.com>
2018-06-01 15:38:39 +0200

    error

--- public/assets/js/proyecto.js
+++ public/assets/js/proyecto.js
@@ -1,29 +1,29 @@
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     var $megustabtn = $('#megusta-btn');
     var $nomegustabtn = $('#no-megusta-btn');
     var cont = 1;
     renderValoracion();
 
-    $('#colaborar-btn').on('click', function() {
+    $('#colaborar-btn').on('click', function () {
         var proyectoId = this.dataset.proyectoid;
         var _url = '/proyecto/' + proyectoId + '/colaborar';
         var $this = $(this);
-        
+
 
         $.ajax({
             url: _url,
             type: 'GET',
             dataType: 'JSON',
-            success: function(data) {
-                if(data.peticion_envidada) {
+            success: function (data) {
+                if (data.peticion_envidada) {
                     $this.css('color', 'yellow');
                 } else {
                     $this.css('color', 'red');
                 }
             },
-            error: function(data) {
+            error: function (data) {
                 console.log(data.responseJSON.error);
                 $this.css('color', 'red');
             }
@@ -31,29 +31,29 @@ $(document).ready(function(){
 
         return false;
     });
-    
+
     /**
- * muestra la valoracion que le habia dado
- * el usuario de la sesion al haberle dado a megusta o no me gusta
- * si no hay sesion iniciada esto no va a hacer nada
- */
-function renderValoracion() {
-    var $megustabtn = $('#no-megusta-btn');
-    var $nomegustabtn = $('#megusta-btn');
-    var $valoracionbtns = $('.valoracion-btn');
-    var megusta = $megustabtn.data('valoracion');
-    var nomegusta = $nomegustabtn.data('valoracion');
+     * muestra la valoracion que le habia dado
+     * el usuario de la sesion al haberle dado a megusta o no me gusta
+     * si no hay sesion iniciada esto no va a hacer nada
+     */
+    function renderValoracion() {
+        var $megustabtn = $('#no-megusta-btn');
+        var $nomegustabtn = $('#megusta-btn');
+        var $valoracionbtns = $('.valoracion-btn');
+        var megusta = $megustabtn.data('valoracion');
+        var nomegusta = $nomegustabtn.data('valoracion');
 
-    if(typeof megusta !== 'undefined' && typeof nomegusta !== 'undefined') {
-        $valoracionbtns.removeClass('valoracion-active');
+        if (typeof megusta !== 'undefined' && typeof nomegusta !== 'undefined') {
+            $valoracionbtns.removeClass('valoracion-active');
 
-        if(megusta === 1 && nomegusta === 0) {
-            $megustabtn.addClass('valoracion-active');
-        } else if(megusta === 0 && nomegusta === 1) {
-            $nomegustabtn.addClass('valoracion-active');
+            if (megusta === 1 && nomegusta === 0) {
+                $megustabtn.addClass('valoracion-active');
+            } else if (megusta === 0 && nomegusta === 1) {
+                $nomegustabtn.addClass('valoracion-active');
+            }
         }
     }
-}
 
     /**
      * Quita la valoracion del proyecto
@@ -63,23 +63,23 @@ function renderValoracion() {
         var $valoracionbtns = $('.valoracion-btn');
 
         $.ajax({
-           url: '/proyecto/' + proyecto + '/quitar_valoracion',
-           type: 'GET',
-           dataType: 'JSON',
-           success: function(data) {
-               $('.valoracion-btn').removeClass('valoracion-active');
-               $valoracionbtns.data('valoracion', 0);
-               console.log('Valoracion quitada');
-           },
-           error: function(data) {
-               var response = JSON.parse(data.responseText);
-               console.log(response.error);
-           }
+            url: '/proyecto/' + proyecto + '/quitar_valoracion',
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (data) {
+                $('.valoracion-btn').removeClass('valoracion-active');
+                $valoracionbtns.data('valoracion', 0);
+                console.log('Valoracion quitada');
+            },
+            error: function (data) {
+                var response = JSON.parse(data.responseText);
+                console.log(response.error);
+            }
         });
     }
 
 
-    $("#borrar-proyecto").on("click",function(){
+    $("#borrar-proyecto").on("click", function () {
         var name = $(this).attr("name");
 
         var BorrarUrl = '/proyecto/' + name + '/eliminar';
@@ -96,100 +96,121 @@ function renderValoracion() {
         });
     });
 
-    $('#project_etiquetas').on('keydown', function(){
+    $('#project_etiquetas').on('keydown', function () {
         var nombreTag = $(this).val();
 
         $.ajax({
-            url: '/tags/' + nombreTag ,
+            url: '/tags/' + nombreTag,
             type: 'GET',
             dataType: 'JSON',
-            success: function(data) {
+            success: function (data) {
                 // TODO poner en un ul los tags
                 console.log(data);
             },
-            error: function(data) {
+            error: function (data) {
                 console.log(data.responseJSON.error);
             }
         });
     });
 
-    $('#no-megusta-btn').on('click', function(){
+    $('#no-megusta-btn').on('click', function () {
         var $this = $(this);
         var proyecto = $this.data('proyectoid');
         var nomegusta = $this.data('valoracion');
 
-        if(nomegusta === 1) {
+        if (nomegusta === 1) {
             quitarValoracion(proyecto);
             return false;
         }
 
         $.ajax({
-           url: '/proyecto/' + proyecto + '/desvalorar',
-           type: 'GET',
-           dataType: 'JSON',
-           success: function(data) {
+            url: '/proyecto/' + proyecto + '/desvalorar',
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (data) {
                 console.log(data);
-                if(data.valorado) {
+                if (data.valorado) {
                     $megustabtn.data('valoracion', 0);
                     $nomegustabtn.data('valoracion', 1);
                     renderValoracion();
                 } else {
                     quitarValoracion(proyecto);
                 }
-           }
-           ,
-           error: function(data) {
-               console.log(data.responseJSON.error);
-           }
-       });
+            }
+            ,
+            error: function (data) {
+                console.log(data.responseJSON.error);
+            }
+        });
     });
 
-    $('#megusta-btn').on('click', function(){
+    $('#megusta-btn').on('click', function () {
         var $this = $(this);
         var proyecto = $this.data('proyectoid');
         var megusta = $this.data('valoracion');
 
-        if(megusta === 1) {
+        if (megusta === 1) {
             quitarValoracion(proyecto);
             return false;
         }
 
         $.ajax({
-           url: '/proyecto/' + proyecto + '/valorar',
-           type: 'GET',
-           dataType: 'JSON',
-           success: function(data) {
+            url: '/proyecto/' + proyecto + '/valorar',
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (data) {
                 console.log(data);
-                if(data.valorado) {
+                if (data.valorado) {
                     $megustabtn.data('valoracion', 1);
                     $nomegustabtn.data('valoracion', 0);
                     renderValoracion();
                 } else {
                     quitarValoracion(proyecto);
                 }
-           },
-           error: function(data) {
-               console.log(data.responseJSON.error);
-           }
+            },
+            error: function (data) {
+                console.log(data.responseJSON.error);
+            }
         });
     });
-    
-    $("#next").on("click",function()
-    {     
-        if(cont < $(".newProyect > form:nth-child(1) > div").length)
+    //$("#next").css("display", "none");
+    $("#next").on("click", function ()
+    {
+        if (cont < $(".newProyect > form:nth-child(1) > div").length)
         {
-            $(".newProyect > form:nth-child(1) > div:nth-child("+cont+")").hide("slow");
+
+            $(".newProyect > form:nth-child(1) > div:nth-child(" + cont + ")").css("display", "none");
             cont++;
-            $(".newProyect > form:nth-child(1) > div:nth-child("+cont+")").show("slow");
+            $(".newProyect > form:nth-child(1) > div:nth-child(" + cont + ")").css("display", "flex");
+
         }
     })
-    $("#previous").on("click",function()
-    {     
-        if(cont > 1)
+    $("#previous").on("click", function ()
+    {
+        if (cont > 1)
         {
-            $(".newProyect > form:nth-child(1) > div:nth-child("+cont+")").hide("slow");
+            $(".newProyect > form:nth-child(1) > div:nth-child(" + cont + ")").css("display", "none");
             cont--;
-            $(".newProyect > form:nth-child(1) > div:nth-child("+cont+")").show("slow");
+            $(".newProyect > form:nth-child(1) > div:nth-child(" + cont + ")").css("display", "flex");
+
         }
-    })
+
+    });  
+    $('#previous').css("display", "none");
+    $('#nuevoProjecto>div:nth-child(1)>div>input').click(function(){
+        
+        if (cont != 1 && cont != $(".newProyect > form:nth-child(1) > div").length) {
+            $("#next").css("display", "block");
+            $('#previous').css("display", "block");
+
+        } else if (cont == 1) {
+            $("#next").css("display", "block");
+            $('#previous').css("display", "none");
+        } else if (cont == $(".newProyect > form:nth-child(1) > div").length) {
+            $("#next").css("display", "none");
+            $('#previous').css("display", "block");
+        }
+    });
+    
+
 });
