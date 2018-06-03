9b9fbfe6cc3bae35a247c618da79f05b092489aa
MagonxESP <janma.360@gmail.com>
2018-06-03 17:23:44 +0200

    confirms

--- src/Controller/ProyectController.php
+++ src/Controller/ProyectController.php
@@ -55,17 +55,6 @@ class ProyectController extends Controller {
         ]);
     }
 
-    private function subirImagen(UploadedFile $file) {
-        if($file->getClientSize() <= UploadedFile::getMaxFilesize()) {
-            $filename = md5(uniqid()).'.'.$file->guessExtension();
-            $path = $this->getParameter('public_img_directory');
-            $file->move($path, $filename);
-            return $path.'/'.$filename;
-        }
-
-        return null;
-    }
-
     /**
      * Crea un proyecto nuevo
      * @param Request $peticion
@@ -126,6 +115,7 @@ class ProyectController extends Controller {
         
         return $proyectos;
     }
+
     /**
      * Renderiza la pagina del proyecto
      * @param Project $proyecto
